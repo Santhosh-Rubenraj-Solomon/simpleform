e7e4b5c4476a3265ac946cf831996f3bb56e6882
rexilia <51748148+rexilia@users.noreply.github.com>
2022-08-10 12:12:45 +0530

    Fixed firebase error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,7 @@
 import { Component } from '@angular/core';
-import {FormControl,FormGroup} from '@angular/forms';
-import {FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'form';
- inputForm = this.fb.group({
-    name: [''],
-    age:  [''],
-    gender: ['']
-})
-    
- onSubmit() {
-  console.warn(this.inputForm.value);
-} 
- constructor(private fb: FormBuilder){}
-}
+export class AppComponent {}
